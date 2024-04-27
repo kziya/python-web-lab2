c0bb7714df991daeae1a4998dce863810a3ace35
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-27 16:08:21 +0300

    add: psycopg to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 
-from .db import Base, engine
 from .food import food_router
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
--- app/db.py
+++ app/db.py
@@ -1,17 +1,17 @@
-from app import config
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import os
 
-engine = create_engine(config.DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+import psycopg
 
-Base = declarative_base()
+connection = psycopg.connect(user=os.getenv("DB_USER"),
+                             password=os.getenv("DB_PASSWORD"),
+                             host=os.getenv('DB_HOST'),
+                             port=os.getenv('DB_PORT'),
+                             dbname=os.getenv('DB_NAME')
+                             )
+
+
+# Create tables
 
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    return connection.cursor()
