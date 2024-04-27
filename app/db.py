62c0474c46d27e827d027bef6ba76af3143fe642
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-27 16:21:59 +0300

    add: Table creation

--- app/db.py
+++ app/db.py
@@ -2,6 +2,10 @@ import os
 
 import psycopg
 
+from .food.food_table import createFoodTable
+from .order.order_table import createOrderTable
+from .user.user_table import createUserTable
+
 connection = psycopg.connect(user=os.getenv("DB_USER"),
                              password=os.getenv("DB_PASSWORD"),
                              host=os.getenv('DB_HOST'),
@@ -10,8 +14,13 @@ connection = psycopg.connect(user=os.getenv("DB_USER"),
                              )
 
 
-# Create tables
-
-
 def get_db():
     return connection.cursor()
+
+
+# Create tables
+createFoodTable(connection)
+createUserTable(connection)
+createOrderTable(connection)
+
+connection.commit()
